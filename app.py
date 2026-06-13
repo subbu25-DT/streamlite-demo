499a26ed5da5e1d1bc42df0ba39ecee64bccc33c
subbu25-DT <subhashsikhakolli1712@gmail.com>
2026-06-13 04:43:56 +0000

    hi

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-import streamlit as st
-st.title(“Welcome to Streamlit”)
-name = st.text_input(“Enter your name”)
-if name: st.success(f “Hello {name}”)
-age = st.slider(“Select your age”, 1, 100)
-st.write(“Age:”, age)
-if st.button(“Celebrate”): st.balloons()
+import streamlit as st  # type: ignore[import]
+
+st.title("Welcome to Streamlit")
+
+name = st.text_input("Enter your name")
+if name:
+	st.success(f"Hello {name}")
+
+age = st.slider("Select your age", 1, 100)
+st.write("Age:", age)
+
+if st.button("Celebrate"):
+	st.balloons()
